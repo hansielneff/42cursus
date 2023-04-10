8d72fa6821dcd28e9fa0d933596519bd8f16ee69
helneff <helneff@student.hive.fi>
2023-04-10 14:36:12 +0300

    Removed executable and fixed tests

--- cpp_modules/cpp_module_02/ex02/main.cpp
+++ cpp_modules/cpp_module_02/ex02/main.cpp
@@ -5,15 +5,15 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b(Fixed( 5.05f ) * Fixed( 2 ));
-	Fixed const c(Fixed( 5.05f ) / Fixed( 0 ));
 
-	std::cout << c << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+
 	std::cout << b << std::endl;
+
 	std::cout << Fixed::max(a, b) << std::endl;
 
 	return 0;
